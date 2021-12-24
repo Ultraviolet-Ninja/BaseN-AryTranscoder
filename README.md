17e966735e168b752cf8e0de2c93dcc6bbad3240
jasmine-jragon <malcolmrjohns740@gmail.com>
2021-12-24 12:56:48 -0600

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# BaseN-AryTranscoder
+# Base N-Ary Transcoder
+
+### Description
+Like Binary, Hexadecimal, or Base64, this was made to encode numbers and decode Strings using user-defined Strings
---
+++ src/transcode/BaseNaryTranscoder.java
@@ -0,0 +1,40 @@
+package transcode;
+
+public class BaseNaryTranscoder {
+    private final char[] codexString;
+
+    public BaseNaryTranscoder(String codexString) {
+        this.codexString = codexString.toCharArray();
+    }
+
+    public String encrypt(int value) {
+        if (value < codexString.length) { return String.valueOf(codexString[value]);}
+
+        StringBuilder sb = new StringBuilder();
+        int size = codexString.length;
+        while (value >= size) {
+            int modIndex = value % size;
+            sb.append(codexString[modIndex]);
+            value /= size;
+        }
+
+        return sb.append(codexString[value])
+                .reverse()
+                .toString();
+    }
+
+    public int decrypt(String str) {
+        int degree = str.length() - 1;
+        int output = 0;
+        String searchString = String.valueOf(codexString);
+
+        for (char letter : str.toCharArray()) {
+            int value = searchString.indexOf(letter);
+            if (value < 0) throw new IllegalArgumentException("Detected letter not in the codex");
+            output += (int) Math.pow(codexString.length, degree--) * value;
+        }
+        return output;
+    }
+
+    public int getBase() {return codexString.length;}
+}
